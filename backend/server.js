be7a5c0fbb6936d03bab122c35e65437ef329dbc
Kshitiz Bhargava <kshitiz.bhargava@siemens.com>
2020-02-11 13:59:13 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const morgan=require('morgan');
+const bodyParser=require('body-parser');
+const cookieParser=require('cookie-parser');
+const cors=require('cors');
+require('dotenv').config();
+
+
+//app
+const app =express()
+
+//Middlewares
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+//cors
+app.use(cors())
+
+//routes
+app.get('/api',(req,res)=>{
+    res.json({time:Date().toString()})
+})
+
+//Port
+
+const port =process.env.PORT || 5000
+app.listen(port,()=>{console.log(`Server is running on ${port}`)})
