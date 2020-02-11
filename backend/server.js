db0c5684cbf54b33e64fc6bc0a2c59e08f58f3a9
NachtBlade31 <kbgoku@gmail.com>
2020-02-11 18:40:57 +0530

    basic setup for backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
-const express=require('express');
-const morgan=require('morgan');
-const bodyParser=require('body-parser');
-const cookieParser=require('cookie-parser');
-const cors=require('cors');
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const connectDB = require('./config/db');
 require('dotenv').config();
+const blogRoutes = require('./routes/blog');
 
 
 //app
-const app =express()
+const app = express()
 
 //DB Connection
 connectDB();
@@ -18,18 +19,17 @@ app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(cookieParser())
 
+
+
 //cors
-if(process.env.NODE_ENV=='development'){
-    app.use(cors({origin:`${process.env.CLIENT_URL}`}))
+if (process.env.NODE_ENV == 'development') {
+    app.use(cors({ origin: `${process.env.CLIENT_URL}` }))
 }
 
-
-//routes
-app.get('/api',(req,res)=>{
-    res.json({time:Date().toString()})
-})
+//Routes Middleware
+app.use('/api', blogRoutes);
 
 //Port
 
-const port =process.env.PORT || 5000
-app.listen(port,()=>{console.log(`Server is running on ${port}`)})
+const port = process.env.PORT || 5000
+app.listen(port, () => { console.log(`Server is running on ${port}`) })
